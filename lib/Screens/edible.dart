9606444d1e5ab11749152466b0aa0d14b3355ec3
Akanksha1212 <akankshasingh121297@gmail.com>
2020-12-06 12:55:45 +0530

    Added csv implementation

--- lib/Screens/edible.dart
+++ lib/Screens/edible.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+import 'package:ttny/Screens/edibleDetail.dart';
+import 'package:ttny/Screens/tensorflow.dart';
 
 class Edible extends StatelessWidget {
   @override
@@ -67,12 +73,12 @@ class Edible extends StatelessWidget {
                       ),
                     ),
                     onTap: () {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) => Scan(),
-                      //   ),
-                      // );
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EdibleDetail(),
+                        ),
+                      );
                     },
                   ),
                   SizedBox(
@@ -83,7 +89,14 @@ class Edible extends StatelessWidget {
                     width: 170,
                     child: RaisedButton.icon(
                       elevation: 15,
-                      onPressed: null,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Tensorflow(),
+                          ),
+                        );
+                      },
                       shape: RoundedRectangleBorder(
                           borderRadius:
                               BorderRadius.all(Radius.circular(10.0))),
@@ -114,5 +127,103 @@ class Edible extends StatelessWidget {
         ),
       ),
     ));
+  }
+}
+
+class Identify extends StatefulWidget {
+  @override
+  _IdentifyState createState() => _IdentifyState();
+}
+
+class _IdentifyState extends State<Identify> {
+  List _outputs;
+  File _image;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _loading
+          ? Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _image == null ? Container() : Image.file(_image),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  _outputs != null
+                      ? Text(
+                          "${_outputs[0]["label"]}",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.0,
+                            background: Paint()..color = Colors.white,
+                          ),
+                        )
+                      : Container()
+                ],
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: pickImage,
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+
+  pickImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(image);
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
   }
 }
---
+++ lib/Screens/edibleDetail.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+class EdibleDetail extends StatefulWidget {
+  @override
+  EdibleDetailState createState() => new EdibleDetailState();
+}
+
+class EdibleDetailState extends State<EdibleDetail> {
+  double screenHeight;
+
+  final GlobalKey<ScaffoldState> scaffoldState = GlobalKey();
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<List<dynamic>> csvTable = new List();
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    return FutureBuilder<String>(
+        future: rootBundle.loadString('assets/Edible.csv'), //
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          List<List<dynamic>> csvTable =
+              CsvToListConverter().convert(snapshot.data);
+          return Scaffold(
+            key: scaffoldKey,
+            backgroundColor: Colors.white,
+            body: Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    new Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                      child: new ListTile(
+                        leading: new Icon(Icons.search),
+                        title: new TextField(
+                          decoration: new InputDecoration(
+                            hintText: 'Search',
+                          ),
+                        ),
+                      ),
+                    ),
+                    new Expanded(
+                      child: ListView.builder(
+                          padding: const EdgeInsets.all(8),
+                          itemCount: csvTable.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Card(
+                              child: ListTile(
+                                leading: Image.network("${csvTable[index][1]}"),
+                                title: Text(
+                                  '${csvTable[index][0]}',
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 25, vertical: 20),
+                                subtitle: Text(' ${csvTable[index][2]} }'),
+                                isThreeLine: true,
+                              ),
+                              color: Colors.blue[100],
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(55),
+                              ),
+                            );
+                          }),
+                    ),
+                  ]),
+            ),
+          );
+        });
+  }
+}
